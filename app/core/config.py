724ea1b4877403acb97cee4abcb441a7e78d882b
Misha Kutsov <misha@misha.ru>
2025-08-06 16:11:51 +0500

    [ADD] redis field and pwd context

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
 import os
 from pydantic_settings import BaseSettings
+from passlib.context import CryptContext
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str 
-
+    REDIS_URL: str
+    
     SECRET_KEY: str 
     ALGORITHM: str 
 
@@ -12,3 +14,5 @@ class Settings(BaseSettings):
         env_file = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
 
 settings = Settings()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
